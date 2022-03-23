95ccdf30b6713f1fa96d705fdc8b739d137bc673
yuki74w <16777216bit@gmail.com>
2022-03-24 02:38:36 +0900

    add reducer.hpp

---
+++ reducer.hpp
@@ -0,0 +1,11 @@
+#ifndef INCLUDED_REDUCER_HPP
+#define INCLUDED_REDUCER_HPP
+#include "lambda.hpp"
+#include "lexer.hpp"
+#include <vector>
+
+Expression reduce(const std::vector<lex_unit> &lex_units)
+{
+}
+
+#endif
