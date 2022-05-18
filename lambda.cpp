a0fd2d39ff3cd132135d0019f500009a709af92f
yuki74w <16777216bit@gmail.com>
2022-05-19 02:45:02 +0900

    add file input mode

--- lambda.cpp
+++ lambda.cpp
@@ -1,23 +1,43 @@
 #include "lambda.hpp"
 #include "reducer.hpp"
+#include <fstream>
 #include <iostream>
 
-int main()
+Expression parseandreduce(std::string_view str)
+{
+    auto l = reduce(lexer(str));
+    auto tmp = l.beta_reduction();
+    while (l.str() != tmp.str())
+    {
+        l = tmp;
+        tmp = tmp.beta_reduction();
+    }
+    l = tmp;
+    return l;
+}
+
+int main(int argc, char **argv)
 {
     std::string str = "";
+    if (argc == 2)
+    {
+        std::ifstream ifs(argv[1]);
+        if (!ifs)
+        {
+            std::cout << "not found: " << argv[1] << std::endl;
+            return 1;
+        }
+        else
+        {
+            std::cout << parseandreduce(str).str() << std::endl;
+            return 0;
+        }
+    }
     while (1)
     {
         std::cout << "λ>";
         std::cin >> str;
-        auto l = reduce(lexer(str));
-        auto tmp = l.beta_reduction();
-        while (l.str() != tmp.str())
-        {
-            l = tmp;
-            tmp = tmp.beta_reduction();
-        }
-        l = tmp;
-        std::cout << l.str() << "\n";
+        std::cout << parseandreduce(str).str() << std::endl;
     }
     return 0;
 }
